8916942d22cb3e5cf913ea8654c04ea11aa876b5
satriakurniawan16 <rahmad16@gmail.com>
2018-11-22 17:03:59 +0700

    Splash and Login Screen

---
+++ app/src/main/java/com/agreader/screen/LoginScreen.java
@@ -0,0 +1,17 @@
+package com.agreader.screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.agreader.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+
+    
+}
---
+++ app/src/main/java/com/agreader/screen/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.agreader.screen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.agreader.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginScreen.class));
+                finish();
+            }
+        },3000L);
+
+    }
+}
