ad1ef7fc0dd6e966cf1b4999d2698691eab68ccd
satriakurniawan16 <rahmad16@gmail.com>
2018-11-29 11:16:26 +0700

    new

--- app/src/main/java/com/agreader/screen/SliderActivity.java
+++ app/src/main/java/com/agreader/screen/SliderActivity.java
@@ -157,12 +157,12 @@ public class SliderActivity extends AppCompatActivity {
     };
 
     private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
     }
+
+
 
     public class MyViewPagerAdapter extends PagerAdapter {
         private LayoutInflater layoutInflater;
