f1ec9a8b706afdf2aa9a5f9fe26c685dcf5aac5f
mtaufiqramadhan <ramadhan.taufiqm@gmail.com>
2018-12-14 14:48:08 +0700

    Fake Report

--- app/src/main/java/com/agreader/screen/UnverifiedProductActivity.java
+++ app/src/main/java/com/agreader/screen/UnverifiedProductActivity.java
@@ -1,5 +1,6 @@
 package com.agreader.screen;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.agreader.R;
+import com.agreader.ReportActivity;
 
 public class UnverifiedProductActivity extends AppCompatActivity {
+
+    Button unverified_report;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,16 @@ public class UnverifiedProductActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
+
+        unverified_report = (Button) findViewById(R.id.unverified_report) ;
+        unverified_report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UnverifiedProductActivity.this, ReportActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         changeStatusBarColor();
     }
