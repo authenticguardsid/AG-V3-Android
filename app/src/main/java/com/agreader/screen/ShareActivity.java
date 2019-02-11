9c0cfd199f81be8cd29beb4658cd1606b7d50e6e
satriakurniawan16 <rahmadsatria16@gmail.com>
2019-02-11 10:32:37 +0700

    "update senin"

--- app/src/main/java/com/agreader/screen/ShareActivity.java
+++ app/src/main/java/com/agreader/screen/ShareActivity.java
@@ -2,14 +2,20 @@ package com.agreader.screen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.agreader.R;
 
 public class ShareActivity extends AppCompatActivity {
 
+    TextView shareText;
+    Button buttonHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
     }
+
+
 }
